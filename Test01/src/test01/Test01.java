ebe9f2621cd1c9fd27352501ef64de3d40895dea
Shiyw1034 <27117991syw>
2020-05-12 21:46:40 +0800

    Initial commit

---
+++ Test01/src/test01/Test01.java
@@ -0,0 +1,11 @@
+package test01;
+
+/**
+ * @author shiyawei
+ * @date 2020/5/12 - 21:39
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("11111111");
+    }
+}
